9f484c1b87a264dc2cf738cebd72881564f42767
Flamingo811 <129621208+Flamingo811@users.noreply.github.com>
2023-05-17 00:55:44 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) =>{
+    res.send('Hello World')
+});
+
+app.get('/student', (req, res) =>{
+    res.send('Hello Student')
+});
+
+app.get('/contact', (req, res) =>{
+    res.send('My contact details are')
+});
+
+app.listen(port, () => {console.log(`Example app listening on port ${port}`)})
